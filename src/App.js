95a26b792fd2f508d637ab404b009e765adb60b9
Henry Chon 2 <chonhenry@outlook.com>
2021-10-08 15:33:32 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Menu } from "antd";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <nav className="navbar">
+      <ul className={`menu ${open ? "open" : ""}`}>
+        <li className="menu-tiem">
+          <a className="menu-link" href="">
+            <span className="menu-num">01.</span> Experience
+          </a>
+        </li>
+        <li className="menu-tiem">
+          <a className="menu-link" href="">
+            <span className="menu-num">02.</span> Projects
+          </a>
+        </li>
+        <li className="menu-tiem">
+          <a className="menu-link" href="">
+            <span className="menu-num">03.</span> Contact
+          </a>
+        </li>
+      </ul>
+      {open ? (
+        <i class="fas fa-times" onClick={toggle}></i>
+      ) : (
+        <i class="fas fa-bars" onClick={toggle}></i>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
